ccf266f633683cbe3541a2c851974bc7058d0f24
Andy Knudsen <andy@websitetalkingheads.com>
2017-06-14 00:23:41 -0600

    Starting new Dreamweaver git

---
+++ includes/footer.php
@@ -0,0 +1,31 @@
+<footer class="bg-primary">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2 class="text-center"><a href="http://talkingheads.com/affiliate/">Powered by <span class="blue">Talking</span> Heads<span class="tm">&#174;</span></a></h2>
+      <div class="quotes">
+        <p id="changeQuote" class="text-center"></p>
+      </div>
+      <h3 class="text-center"><span class="white thin">Talking</span>Heads<span class="tm">&#174;</span></h3>
+    </div>
+    <div class="col-sm-4 text-center">
+      <h2 class="thin">Why Video?</h2>
+      <br>
+      <h3 class="italics thin"><a class="white" href="tel://801-748-2281" title="Let's Talk!" >"It's <span class="font-weight-bold">Time</span> We Talk..."</a></h3>
+    </div>
+    <div class="col-sm-4">
+      <h2 class="text-center">Our Contact Information</h2>
+      <ul class="list-unstyled list-footer text-left white">
+        <li>
+          <h3 class="text-center"><i class="fa fa-phone"></i> <br/>
+            <span><a class="white" href="tel://801-748-2281" title="Give us a call." >801-748-2281</a></span></h3>
+        </li>
+        <li>
+          <h3 class="text-center"><i class="fa fa-envelope-o"></i><br/>
+            <a href="mailto:info@websitetalkingheads.com">info@websitetalkingheads.com</a></h3>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <h3 class="rights"><span class="cw">&#169;</span>Talking Heads <span id="year"></span>. All rights reserved.</h3>
+</footer>
+<script src="js/quotes.js"></script>
